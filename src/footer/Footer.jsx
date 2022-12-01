b78e3a3943f5d23703aa99da66e9fd3aa9f413b2
Alexej <kovernikov@post.cz>
2022-12-01 22:21:15 +0100

    add social sites

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './Footer.module.css';
+import Nav from "../nav/Nav";
+
 
 function Footer() {
 	return (
 		<div className={style.footer}>
-
+			<Nav/>
 		</div>
 	);
 }
