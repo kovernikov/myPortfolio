52df7099d198cc065c509a85941394332c18a105
Alexej <kovernikov@post.cz>
2022-12-07 22:43:49 +0100

    add bloc main

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import Header from "./header/Header";
 import Contact from "./kontakt/Contact";
 import Footer from "./footer/Footer";
+import Main from "./main/Main";
 
-function Gallery() {
-    return null;
-}
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Main/>
             <Contact/>
             <Footer/>
         </div>
---
+++ src/main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Main.module.css';
+
+function Main() {
+
+    return (
+        <div className={style.main}>
+            <div>Hello, I'm Alexey</div>
+            <div><img src={""}/></div>
+        </div>
+    );
+}
+
+export default Main;
